3927cb0537c5abbc7036fe75a3fa68093454fd59
King AKidi <onlinemraam@gmail.com>
2022-06-02 16:36:56 +0100

    first commit

---
+++ database/migrations/2022_06_02_124052_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('post_user_id');
+            $table->string("post_title");
+            $table->text("post_content");
+            $table->text("post_images");
+            $table->string('post_slug');
+            $table->string('post_status')->default('approved');
+            $table->boolean('post_comment_status')->default(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
